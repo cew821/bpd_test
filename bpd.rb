048633bcd764612d3827bdd9d0695b930da097b6
Charles Worthignton <c.e.worthington@gmail.com>
2013-09-16 10:56:37 -0400

    bpd starter

--- bpd.rb
+++ bpd.rb
@@ -1,28 +1,29 @@
 require 'json'
 require 'httparty'
 
-creditentials = YAML::load_file(File.join(__dir__, 'config.yml'))
+credentials = YAML::load_file(File.join(__dir__, 'config.yml'))
 
-puts nf
+puts credentials["BPD_USER_NAME"]
+puts credentials["BPD_API_KEY"]
 
-# url = "https://be-denali.herokuapp.com/api/v1/analyze/peers/"
+url = "https://be-denali.herokuapp.com/api/v1/analyze/peers/"
 
-# payload = {
-#     'filters' => {
-#         'state' => ['CA', 'IL' 'OR'],
-#         'facility-type' => ['Retail - Big Box (> 50K sf)']
-#     },
-#     'number_of_bins' => 25
-# }
+payload = {
+    'filters' => {
+        'state' => ['CA', 'IL' 'OR'],
+        'facility-type' => ['Retail - Big Box (> 50K sf)']
+    },
+    'number_of_bins' => 25
+}
 
-# headers = {
-#     "Content-Type" => "application/json", 
-#     "Authorization" => "ApiKey #{ BPD_USER_NAME }:#{ BPD_API_KEY }"
-# }
+headers = {
+    "Content-Type" => "application/json", 
+    "Authorization" => "ApiKey #{ credentials["BPD_USER_NAME"] }:#{ credentials["BPD_API_KEY"] }"
+}
 
-# response = HTTParty.post(
-#     url,
-#     :body => payload.to_json,
-#     :headers => headers
-# )
-# puts response
+response = HTTParty.post(
+    url,
+    :body => payload.to_json,
+    :headers => headers
+)
+puts response
